fc77471d578606a189e3f2bf5804ed049c56c994
a924905699 <924905699@qq.com>
2020-06-01 13:33:14 +0800

    1.0

--- java/Solution.java
+++ java/Solution.java
@@ -1,17 +1,15 @@
-class TreeNode {
-    int val;
-     TreeNode left;
-     TreeNode right;
-     TreeNode(int x) { val = x; }
- }
-
 class Solution {
-    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
-        if(root==null||root==p||root==q) return root;
-        TreeNode left=lowestCommonAncestor(root.left,p,q);
-        TreeNode right=lowestCommonAncestor(root.right,p,q);
-        if(left==null) return right;
-        if(right==null) return left;
-        return root;
+    public int maxProduct(int[] nums) {
+        int MAX=nums[0];
+        int MIN=nums[0];
+        int ans=0;
+        for (int i=1;i<nums.length;i++){
+            int MX=MAX;
+            int MN=MIN;
+            MAX=Math.max(MX*nums[i],Math.max(nums[i],MN*nums[i]));
+            MIN=Math.min(MX*nums[i],Math.min(nums[i],MN*nums[i]));
+            ans=Math.max(MAX,ans);
+        }
+        return ans;
     }
 }
